b6ea3d8da567701cce4775a219483c5566ebfdd1
Nadine Hemmeter <nadine-hemmeter222@t-online.de>
2022-05-09 15:54:15 +0200

    initial commit

---
+++ src/main/java/com/example/cookncalc/CookNCalcApplication.java
@@ -0,0 +1,13 @@
+package com.example.cookncalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookNCalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookNCalcApplication.class, args);
+    }
+
+}
