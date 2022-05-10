d536bf6a02637b0f27ab5618988d2e743e9d9aa9
Nadine Hemmeter <nadine-hemmeter222@t-online.de>
2022-05-10 10:52:38 +0200

    Created interface RecipeRepository.java

--- src/main/java/com/example/cookncalc/recipes/RecipeRepository.java
+++ src/main/java/com/example/cookncalc/recipes/RecipeRepository.java
@@ -3,7 +3,9 @@ package com.example.cookncalc.recipes;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
-
+    List<Recipe> findAll();
 }
