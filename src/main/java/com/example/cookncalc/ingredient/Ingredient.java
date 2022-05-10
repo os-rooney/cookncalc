b3d88f9b4dd99005d88ec85159545a2e7351022b
Nadine Hemmeter <nadine-hemmeter222@t-online.de>
2022-05-10 09:50:31 +0200

    Created Class Ingredient.java

---
+++ src/main/java/com/example/cookncalc/ingredient/Ingredient.java
@@ -0,0 +1,61 @@
+package com.example.cookncalc.ingredient;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private Double amount;
+
+    private String unit;
+
+    public Ingredient(){
+
+    }
+
+    public Ingredient(String name, Double amount, String unit){
+        this.name = name;
+        this.amount = amount;
+        this.unit = unit;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+}
