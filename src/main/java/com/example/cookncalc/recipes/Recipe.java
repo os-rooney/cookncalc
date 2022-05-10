65cd1bdfbfb2ed1a127c1635120013e7cd412067
Nadine Hemmeter <nadine-hemmeter222@t-online.de>
2022-05-10 09:50:48 +0200

    Created Class Recipe.java

---
+++ src/main/java/com/example/cookncalc/recipes/Recipe.java
@@ -0,0 +1,78 @@
+package com.example.cookncalc.recipes;
+
+import com.example.cookncalc.ingredient.Ingredient;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.time.Instant;
+import java.util.List;
+
+@Entity
+public class Recipe {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    @OneToMany
+    private List<Ingredient> ingredient;
+
+    private String description;
+
+    private Instant createdAt;
+
+    public Recipe(){
+
+    }
+
+    public Recipe(String title, List<Ingredient> ingredient, String description) {
+        this.title = title;
+        this.ingredient = ingredient;
+        this.description = description;
+        this.createdAt = Instant.now();
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String name) {
+        this.title = name;
+    }
+
+    public List<Ingredient> getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(List<Ingredient> ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
