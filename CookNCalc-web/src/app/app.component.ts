954a5f1ee016ec745bb1a12f18d7a0837741d5fe
Nadine Hemmeter <nadine-hemmeter222@t-online.de>
2022-05-10 11:17:06 +0200

    edited AppComponent

--- CookNCalc-web/src/app/app.component.ts
+++ CookNCalc-web/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Recipe} from "./Recipe";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'CookNCalc-web';
+export class AppComponent implements OnInit{
+
+  recipes?: Recipe[];
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.httpClient.get<Recipe[]>("/api").subscribe(result => this.recipes=result);
+  }
+
+
 }
