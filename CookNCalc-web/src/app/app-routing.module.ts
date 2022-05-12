48d5611565f14a59cf1b16256387c755eef65550
Rooney H. Osman <dev@ronios.de>
2022-05-12 11:36:40 +0200

    add new recipe page to path module

--- CookNCalc-web/src/app/app-routing.module.ts
+++ CookNCalc-web/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {RecipeDetailsComponent} from "./recipe/recipe-details/recipe-details.component";
 import {RecipeComponent} from "./recipe/recipe.component";
+import {RecipeAddComponent} from "./recipe/recipe-add/recipe-add.component";
 
 const routes: Routes = [
   {path:"", redirectTo:"recipes", pathMatch:"full"},
   {path:"recipes", component:RecipeComponent},
-  {path:"recipes/:id", component: RecipeDetailsComponent}
+  {path:"recipes/:id", component: RecipeDetailsComponent},
+  {path:"addRecipe", component: RecipeAddComponent}
 ];
 
 @NgModule({
