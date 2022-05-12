7c4355e43660933670d8af03beec951fc3715f51
Rooney H. Osman <dev@ronios.de>
2022-05-12 09:57:40 +0200

    addition of recipe-add component

--- CookNCalc-web/src/app/app.module.ts
+++ CookNCalc-web/src/app/app.module.ts
@@ -7,12 +7,14 @@ import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 import { RecipeDetailsComponent } from './recipe/recipe-details/recipe-details.component';
 import { RecipeComponent } from './recipe/recipe.component';
+import { RecipeAddComponent } from './recipe/recipe-add/recipe-add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RecipeDetailsComponent,
-    RecipeComponent
+    RecipeComponent,
+    RecipeAddComponent
   ],
   imports: [
     BrowserModule,
