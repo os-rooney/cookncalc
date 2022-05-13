0558ceec57820dc9971b91a7e5aa4707e397fd41
Anja Schiebel <schiebel955@gmail.com>
2022-05-13 08:59:34 +0200

    RecipeRepository.java RecipeController clean Code

--- src/main/java/com/example/cookncalc/recipes/RecipeRepository.java
+++ src/main/java/com/example/cookncalc/recipes/RecipeRepository.java
@@ -10,32 +10,6 @@ import java.util.List;
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
     List<Recipe> findAll();
 
-    @Query("select r.title as Title, i.name as Zutat from Ingredient i, Recipe r, RecipeIngredient ri where ri.ingredient.id = i.id and ri.recipe.id = r.id")
-    List<Object[]> getThis();
-
-    @Query(value = "select r.title as Title, i.name as Zutat from ingredient i, recipe r, recipe_ingredient ri where ri.ingredient_id = i.id and ri.recipe_id = r.id", nativeQuery = true)
-    List<Object[]> getThisNative();
-
-    @Query(value = "select t.Rezept_ID, t.Rezept_Title, t.Markt, sum(t.Preis*t.VPE_Need) as Gesamt_Preis from (select r.id                                                                             as Rezept_ID,\n" +
-            "                      r.title                                                                          as Rezept_Title,\n" +
-            "                      i.name                                                                           as Zutat,\n" +
-            "                      si.price                                                                         as Preis,\n" +
-            "                      s.name                                                                           as Markt,\n" +
-            "                      i.package_size                                                                   as VPE,\n" +
-            "                      ri.amount                                                                        as Rezept_Amount,\n" +
-            "                      floor(ri.amount / i.package_size) + if(mod(ri.amount, i.package_size) > 0, 1, 0) as VPE_Need\n" +
-            "               from ingredient i,\n" +
-            "                    recipe r,\n" +
-            "                    recipe_ingredient ri,\n" +
-            "                    supermarket_ingredient si,\n" +
-            "                    supermarket s\n" +
-            "               where ri.ingredient_id = i.id\n" +
-            "                 and ri.recipe_id = r.id\n" +
-            "                 and si.ingredient_id = i.id\n" +
-            "                 and si.supermarket_id = s.id\n" +
-            "               order by s.name) t GROUP BY t.Rezept_ID, t.Rezept_Title, t.Markt order by t.Rezept_ID, Gesamt_Preis", nativeQuery = true)
-    List<Object[]> getTotalAmountPerMarket();
-
     @Query(value = "select t.Rezept_ID, t.Rezept_Title, t.Markt, t.Zutat, t.VPE_Need, t.Preis * t.VPE_Need as Ges_Produkt_Preis\n" +
             "from (select r.id                                                                             as Rezept_ID,\n" +
             "             r.title                                                                          as Rezept_Title,\n" +
@@ -58,30 +32,8 @@ public interface RecipeRepository extends CrudRepository<Recipe, Long> {
             "order by t.Rezept_ID, t.Markt, t.Zutat", nativeQuery = true)
     List<Object[]> getAmountPerIngredientPerMarket();
 
-
-    @Query(value = "select t.Rezept_ID, t.Rezept_Title, t.Markt, t.Zutat, t.VPE_Need, t.Preis * t.VPE_Need as Ges_Produkt_Preis\n" +
-            "from (select r.id                                                                             as Rezept_ID,\n" +
-            "             r.title                                                                          as Rezept_Title,\n" +
-            "             i.name                                                                           as Zutat,\n" +
-            "             si.price                                                                         as Preis,\n" +
-            "             s.name                                                                           as Markt,\n" +
-            "             i.package_size                                                                   as VPE,\n" +
-            "             ri.amount                                                                        as Rezept_Amount,\n" +
-            "             floor(ri.amount / i.package_size) + if(mod(ri.amount, i.package_size) > 0, 1, 0) as VPE_Need\n" +
-            "      from ingredient i,\n" +
-            "           recipe r,\n" +
-            "           recipe_ingredient ri,\n" +
-            "           supermarket_ingredient si,\n" +
-            "           supermarket s\n" +
-            "      where ri.ingredient_id = i.id\n" +
-            "        and ri.recipe_id = r.id\n" +
-            "        and si.ingredient_id = i.id\n" +
-            "        and si.supermarket_id = s.id\n" +
-            "      order by s.name) t where t.Rezept_ID = ?1\n" +
-            "order by t.Rezept_ID, t.Markt, t.Zutat", nativeQuery = true)
-    List<Object[]> getAmountPerIngredientPerMarketForRecipeId(Long id);
-
-    @Query(value = "select t.Rezept_ID, t.Rezept_Title, t.Markt, sum(t.Preis*t.VPE_Need) as Gesamt_Preis from (select r.id                                                                             as Rezept_ID,\n" +
+    @Query(value = "select t.Rezept_ID, t.Rezept_Title, t.Markt, sum(t.Preis*t.VPE_Need) as Gesamt_Preis from (" +
+            "               select r.id                                                                             as Rezept_ID,\n" +
             "                      r.title                                                                          as Rezept_Title,\n" +
             "                      i.name                                                                           as Zutat,\n" +
             "                      si.price                                                                         as Preis,\n" +
@@ -100,4 +52,26 @@ public interface RecipeRepository extends CrudRepository<Recipe, Long> {
             "                 and si.supermarket_id = s.id\n" +
             "               order by s.name) t where t.Rezept_ID = ?1 GROUP BY t.Rezept_ID, t.Rezept_Title, t.Markt order by t.Rezept_ID, Gesamt_Preis", nativeQuery = true)
     List<Object[]> getTotalAmountPerMarketForRecipeId(Long id);
+
+      /* @Query(value = "select t.Rezept_ID, t.Rezept_Title, t.Markt, t.Zutat, t.VPE_Need, t.Preis * t.VPE_Need as Ges_Produkt_Preis\n" +
+             "from (select r.id                                                                             as Rezept_ID,\n" +
+             "             r.title                                                                          as Rezept_Title,\n" +
+             "             i.name                                                                           as Zutat,\n" +
+             "             si.price                                                                         as Preis,\n" +
+             "             s.name                                                                           as Markt,\n" +
+             "             i.package_size                                                                   as VPE,\n" +
+             "             ri.amount                                                                        as Rezept_Amount,\n" +
+             "             floor(ri.amount / i.package_size) + if(mod(ri.amount, i.package_size) > 0, 1, 0) as VPE_Need\n" +
+             "      from ingredient i,\n" +
+             "           recipe r,\n" +
+             "           recipe_ingredient ri,\n" +
+             "           supermarket_ingredient si,\n" +
+             "           supermarket s\n" +
+             "      where ri.ingredient_id = i.id\n" +
+             "        and ri.recipe_id = r.id\n" +
+             "        and si.ingredient_id = i.id\n" +
+             "        and si.supermarket_id = s.id\n" +
+             "      order by s.name) t where t.Rezept_ID = ?1\n" +
+             "order by t.Rezept_ID, t.Markt, t.Zutat", nativeQuery = true)
+     List<Object[]> getAmountPerIngredientPerMarketForRecipeId(Long id);*/
 }
