c15a1f49b2ef391384526fe9828104382d2b7392
Anja Schiebel <schiebel955@gmail.com>
2022-05-16 10:49:01 +0200

    add first draft of security configuration

---
+++ src/main/java/com/example/cookncalc/config/WebSecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.example.cookncalc.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.servlet.http.HttpServletResponse;
+
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/api/users/**").authenticated()
+                .anyRequest().authenticated();
+
+
+        http
+                .logout()
+                .logoutUrl("/api/auth/logout")
+                .deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true)
+                .logoutSuccessHandler((request, response, authentication) -> response.setStatus(HttpServletResponse.SC_OK))
+                .permitAll();
+
+        http
+                .httpBasic().and()
+                .csrf().disable()
+                .cors().and()
+                .formLogin().disable();
+    }
+
+
+
+}
