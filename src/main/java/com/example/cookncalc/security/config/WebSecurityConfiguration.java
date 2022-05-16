6ce94d69a14caf75aa4695bc49337e7fe203e976
Anja Schiebel <schiebel955@gmail.com>
2022-05-16 11:13:36 +0200

    provide PasswordEncoder for registration and login

--- src/main/java/com/example/cookncalc/security/config/WebSecurityConfiguration.java
+++ src/main/java/com/example/cookncalc/security/config/WebSecurityConfiguration.java
@@ -1,15 +1,23 @@
 package com.example.cookncalc.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
