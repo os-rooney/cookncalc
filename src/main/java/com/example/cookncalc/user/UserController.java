38b1ad01693d855d730d6dda152cfb5cf4618ca9
Anja Schiebel <schiebel955@gmail.com>
2022-05-16 11:21:36 +0200

    add api endpoint to retrieve current User

--- src/main/java/com/example/cookncalc/user/UserController.java
+++ src/main/java/com/example/cookncalc/user/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 
@@ -29,4 +30,13 @@ public class UserController {
     public User userById(@PathVariable Long userId) {
         return this.userService.findById(userId);
     }
+
+    @GetMapping("/current")
+    public User current(Principal principal) {
+        if (principal != null) {
+            return this.userService.findByName(principal.getName()).orElseThrow();
+        }
+        return null;
+    }
+
 }
