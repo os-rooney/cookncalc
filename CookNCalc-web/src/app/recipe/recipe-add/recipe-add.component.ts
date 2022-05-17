82d0f46d8b7215544b2f9262ddde86c89b1eb905
Nadine Hemmeter <nadine-hemmeter222@t-online.de>
2022-05-17 09:46:36 +0200

    enable adding recipe with user_id

--- CookNCalc-web/src/app/recipe/recipe-add/recipe-add.component.ts
+++ CookNCalc-web/src/app/recipe/recipe-add/recipe-add.component.ts
@@ -3,6 +3,7 @@ import {Recipe} from "../../Recipe";
 import {HttpClient} from "@angular/common/http";
 import {RecipeIngredient} from "../../RecipeIngredient";
 import {Router} from "@angular/router";
+import {User} from "../../model/user";
 
 @Component({
   selector: 'app-recipe-add',
@@ -14,7 +15,6 @@ export class RecipeAddComponent implements OnInit {
   ingredients?: RecipeIngredient[];
   recipes?: Recipe[];
 
-
   recipe: Recipe = {
     id: 0,
     title: "",
@@ -22,13 +22,16 @@ export class RecipeAddComponent implements OnInit {
       {amount: 0, name:'', unit:''},
     ],
     description: "",
-    preparation: ""
+    preparation: "",
+    user: {id: 0, username:"", admin:true}
   }
+
 
   constructor(private http: HttpClient,private router: Router) { }
 
   ngOnInit(): void {
     this.http.get<RecipeIngredient[]>('api/ingredients').subscribe(list => this.ingredients = list);
+    this.http.get<User>('/api/users/current').subscribe(user => this.recipe.user = user);
   }
 
   saveRecipe(){
