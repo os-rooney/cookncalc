e101f069df6cc0fc16607756650cf347e41e3a2b
Nadine Hemmeter <nadine-hemmeter222@t-online.de>
2022-05-17 12:17:23 +0200

    change OnInit Method for MyRecipe

--- CookNCalc-web/src/app/app.module.ts
+++ CookNCalc-web/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {RecipeEditComponent} from "./recipe/recipe-details/recipe-edit/recipe-ed
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { MyRecipeComponent } from './recipe/my-recipe/my-recipe.component';
+import { MyRecipeDetailsComponent } from './recipe/my-recipe/my-recipe-details/my-recipe-details.component';
 
 
 @NgModule({
@@ -27,7 +28,8 @@ import { MyRecipeComponent } from './recipe/my-recipe/my-recipe.component';
     RecipeEditComponent,
     LoginComponent,
     RegisterComponent,
-    MyRecipeComponent
+    MyRecipeComponent,
+    MyRecipeDetailsComponent
   ],
   imports: [
     BrowserModule,
