1f4f457e123271b5af329a47c1fc1de2b5bd804b
Rebecca Fischer <rebecca.fischer@awacademy.training>
2022-05-17 16:32:17 +0200

    former controllers combined in HomeController

--- src/main/java/com/example/cookncalc/HomeController.java
+++ src/main/java/com/example/cookncalc/HomeController.java
@@ -3,7 +3,10 @@ package com.example.cookncalc;
 import com.example.cookncalc.ingredient.IngredientDTO;
 import com.example.cookncalc.json.RecipeWithIngredientsDTO;
 import com.example.cookncalc.recipes.RecipeDTO;
-import com.example.cookncalc.services.RecipeService;
+import com.example.cookncalc.recipes.RecipeRepository;
+import com.example.cookncalc.recipes.TotalPriceForRecipe;
+import com.example.cookncalc.supermarketIngredient.SupermarketIngredient;
+import com.example.cookncalc.supermarketIngredient.SupermarketIngredientRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,46 +19,62 @@ public class HomeController {
 
     private final static Logger logger = LoggerFactory.getLogger(HomeController.class);
 
-    private final RecipeService recipeService;
+    private final HomeService homeService;
+
+    private final RecipeRepository recipeRepository;
+
+    private final SupermarketIngredientRepository supermarketIngredientRepository;
 
     @Autowired
-    public HomeController(RecipeService recipeService){
-        this.recipeService = recipeService;
+    public HomeController(HomeService homeService, RecipeRepository recipeRepository, SupermarketIngredientRepository supermarketIngredientRepository){
+        this.homeService = homeService;
+        this.recipeRepository = recipeRepository;
+        this.supermarketIngredientRepository = supermarketIngredientRepository;
     }
 
     @GetMapping("/api")
     public List<RecipeDTO> home(){
-        return recipeService.showRecipes();
+        return homeService.showRecipes();
     }
 
-    @GetMapping("/api/myrecipe")
+    @GetMapping("/api/myrecipes")
     public List<RecipeDTO> getMyRecipes(@RequestParam long id, @RequestParam String username, @RequestParam boolean admin){
-        return recipeService.findRecipeByUser(id);
+        return homeService.findRecipeByUser(id);
     }
 
-    @GetMapping("/api/recipe/{id}")
+    @GetMapping("/api/recipes/{id}")
     public RecipeWithIngredientsDTO detail(@PathVariable Long id){
-        return recipeService.showDetailRecipe(id);
+        return homeService.showDetailRecipe(id);
     }
 
-    @PostMapping("/api/addRecipe")
+    @PostMapping("/api/recipes/add")
     public void add(@RequestBody RecipeWithIngredientsDTO dto){
-        recipeService.addRecipe(dto);
+        homeService.addRecipe(dto);
     }
 
-    @GetMapping("/api/ingredients")
-    public List<IngredientDTO> fillDropdown() {
-        return recipeService.ingredientsForDropdown();
-    }
-
-    @DeleteMapping("/api/deleteRecipe/{id}")
+    @DeleteMapping("/api/recipes/{id}/delete")
     public void deleteRecipe(@PathVariable Long id){
-        recipeService.deleteRecipe(id);
+        homeService.deleteRecipe(id);
     }
 
     @PostMapping("/api/recipe/{id}/edit")
     public void change(@PathVariable Long id, @RequestBody RecipeWithIngredientsDTO dto){
-        recipeService.deleteRecipe(id);
-        recipeService.addRecipe(dto);
+        homeService.deleteRecipe(id);
+        homeService.addRecipe(dto);
+    }
+
+    @GetMapping("/api/recipes/{id}/calculation")
+    public List<TotalPriceForRecipe> recipeTest(@PathVariable Long id){
+        return  homeService.priceCalculation(id);
+    }
+
+    @GetMapping("/api/prices/{id}")
+    public List<SupermarketIngredient> getIngredientPrice(@PathVariable("id") Long id){
+        return homeService.getIngredientPrices(id);
+    }
+
+    @GetMapping("/api/ingredients")
+    public List<IngredientDTO> fillDropdown() {
+        return homeService.ingredientsForDropdown();
     }
 }
