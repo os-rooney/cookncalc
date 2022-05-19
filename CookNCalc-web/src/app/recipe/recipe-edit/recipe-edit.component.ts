b375d30e0c346a021369dacbc432f44e7efbbddf
Nadine Hemmeter <nadine-hemmeter222@t-online.de>
2022-05-19 11:21:48 +0200

    methods and html added

--- CookNCalc-web/src/app/recipe/recipe-edit/recipe-edit.component.ts
+++ CookNCalc-web/src/app/recipe/recipe-edit/recipe-edit.component.ts
@@ -3,6 +3,7 @@ import {Recipe} from "../../model/recipe";
 import {ActivatedRoute, Router} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
 import {RecipeIngredient} from "../../model/recipeIngredient";
+import {User} from "../../model/user";
 
 @Component({
   selector: 'app-recipe-change',
@@ -13,25 +14,39 @@ export class RecipeEditComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router) { }
 
-  @Input()
-  recipe?: Recipe;
+  recipe: Recipe = {
+    id: 0,
+    title: "",
+    ingredients: [
+      {amount: 0, name:'', unit:''},
+    ],
+    description: "",
+    preparation: "",
+    user: {id: 3, username:"", admin:true}
+  }
   ingredients?: RecipeIngredient[];
   recipes?:Recipe[];
 
   id?:number;
 
+  user?:User;
+
   ngOnInit(): void {
     this.http.get<RecipeIngredient[]>('api/ingredients').subscribe(list => this.ingredients = list);
     this.id = Number(this.route.snapshot.paramMap.get("id"));
-    this.http.get<Recipe>(`/api/recipes/${this.id}`).subscribe(result => this.recipe = result);
+    this.http.get<Recipe>(`/api/recipes/${this.id}`).subscribe(result => {
+      this.recipe = result;
+      this.http.get<User>('/api/users/current').subscribe(user => this.user = user);
+      },)
   }
 
   changeRecipe(){
+    if (this.user) {
+      this.recipe.user = this.user;
+    }
     this.http.post<Recipe>(`/api/recipes/${this.id}/edit`, this.recipe).subscribe();
-
     this.http.get<Recipe[]>("/api").subscribe(result => this.recipes=result);
     this.router.navigate(['/recipes']);
-
   }
 
   findUnit(ingredientName : string){
